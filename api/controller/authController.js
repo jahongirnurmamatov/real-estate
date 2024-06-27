bb131127278d67c6a6465ad607dcaa86084d2cc4
jahongiir.nurmamatov <jahongiir.nurmamatov@gmail.com>
2024-06-27 16:04:09 +0500

    errorhandler middleware added

--- api/controller/authController.js
+++ api/controller/authController.js
@@ -1,15 +1,22 @@
 import User from "../models/User.js";
 import bcrypt from 'bcryptjs'
+import { errorHandler } from "../utils/error.js";
 
-const signup=async(req,res)=>{
+const signup=async(req,res,next)=>{
     const {username,email,password}=req.body;
+    if(!username||!email||!password){
+        next(errorHandler(550,'Missing fields'));
+    }
+    if(password.length<6){
+        next(errorHandler(550,'Password length must be minumum 6 characters'))
+    }
     const hashedPassword=bcrypt.hashSync(password,10);
     const newUser = new User({username,email,password:hashedPassword});
     try {
         await newUser.save();
         res.status(201).json('User created successfully!')
     } catch (error) {
-       res.status(500).json(error.message) 
+       next(error)
     }
 }
 
--- api/index.js
+++ api/index.js
@@ -13,7 +13,17 @@ mongoose.connect(process.env.MONGO_DB).then(()=>console.log('MongoDB is connecte
 
 //routes
 app.use('/api/user',userRoute);
-app.use('/api/auth',authRoute)
+app.use('/api/auth',authRoute);
+
+app.use((err,req,res,next) =>{
+    const statusCode = err.statusCode||500;
+    const message = err.message||'Internal Server Error';
+    return res.status(statusCode).json({
+        success:false,
+        statusCode,
+        message,
+    })
+})
 
 app.listen(3000, () => {
     console.log('Server is running on port 3000!');
