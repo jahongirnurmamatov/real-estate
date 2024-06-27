2c47b4e863b4d1332495275a29621d3b72e82a58
jahongiir.nurmamatov <jahongiir.nurmamatov@gmail.com>
2024-06-27 17:08:45 +0500

    complete signup functionality

--- api/controller/authController.js
+++ api/controller/authController.js
@@ -6,15 +6,17 @@ const signup=async(req,res,next)=>{
     const {username,email,password}=req.body;
     if(!username||!email||!password){
         next(errorHandler(550,'Missing fields'));
+        return;
     }
     if(password.length<6){
-        next(errorHandler(550,'Password length must be minumum 6 characters'))
+        next(errorHandler(550,'Password length must be minumum 6 characters'));
+        return;
     }
     const hashedPassword=bcrypt.hashSync(password,10);
     const newUser = new User({username,email,password:hashedPassword});
     try {
         await newUser.save();
-        res.status(201).json('User created successfully!')
+        res.status(201).json({success:true,message:'user signed up'})
     } catch (error) {
        next(error)
     }
