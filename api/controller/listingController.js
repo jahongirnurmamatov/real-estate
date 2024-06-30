5c45bf7b505adbf500820546ae684cff40e15f2b
jahongiir.nurmamatov <jahongiir.nurmamatov@gmail.com>
2024-06-30 16:35:03 +0500

    update Listing api route

--- api/controller/listingController.js
+++ api/controller/listingController.js
@@ -5,7 +5,6 @@ import { errorHandler } from "../utils/error.js";
 export const createListing=async(req,res,next)=>{
     try {
         const listing = await Listing.create(req.body);
-        console.log(req.body)
         return res.status(201).json(listing);
     } catch (error) {
         next(error);
@@ -14,7 +13,6 @@ export const createListing=async(req,res,next)=>{
 export const  deleteList=async(req,res,next)=>{
    
     const listing =await Listing.findById(req.params.id);
-    console.log(listing.userRef, req.user.id)
     if(!listing){
         return next(errorHandler(404,'Listing not found!'))
     }
@@ -28,3 +26,23 @@ export const  deleteList=async(req,res,next)=>{
         next(error)
     }
 }
+export const editListing = async (req, res, next) => {
+    try {
+        const listing = await Listing.findById(req.params.id);
+        if (!listing) {
+            return next(errorHandler(404, 'Listing not found!'));
+        }
+        if (req.user.id !== listing.userRef) {
+            return next(errorHandler(401, 'You can only edit your own listing'));
+        }
+
+        const updatedListing = await Listing.findByIdAndUpdate(
+            req.params.id,
+            req.body,
+            { new: true }
+        );
+        res.status(200).json(updatedListing);
+    } catch (error) {
+        next(error);
+    }
+};
--- api/routes/listingRoute.js
+++ api/routes/listingRoute.js
@@ -1,10 +1,11 @@
 import express from 'express';
-import { createListing,deleteList } from '../controller/listingController.js';
+import { createListing,deleteList,editListing } from '../controller/listingController.js';
 import { verifyToken } from '../utils/verifyUser.js';
 
 const listingRouter = express.Router();
 
 listingRouter.post('/create',verifyToken, createListing);
-listingRouter.delete('/delete/:id',verifyToken,deleteList)
+listingRouter.delete('/delete/:id',verifyToken,deleteList);
+listingRouter.post('/edit/:id',verifyToken,editListing);
 
 export default listingRouter;
