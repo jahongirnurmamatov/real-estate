10e39ef39cb4dddbfb847feece474d842190611c
jahongiir.nurmamatov <jahongiir.nurmamatov@gmail.com>
2024-07-01 10:54:58 +0500

    search api route

--- api/routes/listingRoute.js
+++ api/routes/listingRoute.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { createListing,deleteList,editListing,getListing } from '../controller/listingController.js';
+import { createListing,deleteList,editListing,getListing,getListings } from '../controller/listingController.js';
 import { verifyToken } from '../utils/verifyUser.js';
 
 const listingRouter = express.Router();
@@ -8,6 +8,7 @@ listingRouter.post('/create',verifyToken, createListing);
 listingRouter.delete('/delete/:id',verifyToken,deleteList);
 listingRouter.post('/edit/:id',verifyToken,editListing);
 listingRouter.get('/get/:id',getListing);
+listingRouter.get('/get',getListings);
 
 
 export default listingRouter;
