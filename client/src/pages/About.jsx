c9a4efbaf656fedc063974e1744f00ff7a580cf2
jahongiir.nurmamatov <jahongiir.nurmamatov@gmail.com>
2024-07-02 10:18:36 +0500

    complete about page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -2,7 +2,12 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>About</div>
+    <div className='max-w-6xl mx-auto px-4 py-20'>
+      <h1 className='text-3xl font-bold mb-4 text-slate-800'>About Future Estate</h1>
+      <p className='mb-4 text-slate-700'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perferendis, repellat! Expedita explicabo dicta, accusantium, facilis provident dolor qui, consequatur error quia sit eos ipsam. Est quasi iusto eveniet sit commodi!</p>
+      <p className='mb-4 text-slate-700'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Magnam nobis excepturi dolores iusto. Repellat minus consectetur commodi. Pariatur provident perferendis architecto aperiam, voluptate ab quam vero at. Labore, dolorem mollitia!</p>
+      <p className='mb-4 text-slate-700'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi, voluptatum. Doloremque facilis odio incidunt cumque, architecto eligendi voluptas numquam et praesentium odit animi ducimus nihil est sapiente enim dolorem quidem.</p>
+    </div>
   )
 }
 
