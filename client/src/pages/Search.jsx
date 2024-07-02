ff37835542a8a9d41e68c59fe3f78a3498d72a11
jahongiir.nurmamatov <jahongiir.nurmamatov@gmail.com>
2024-07-02 11:00:33 +0500

    search page fixed

--- client/src/pages/Search.jsx
+++ client/src/pages/Search.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import ListingItem from '../components/ListingItem';
-
+import React from 'react';
 
 export default function Search() {
   const navigate = useNavigate();
